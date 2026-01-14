d6bd55c87a977fe9817a5dbda9f49037a45e5286
zegemenim <zegemenim@gmail.com>
2026-01-14 16:07:30 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_moons
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import seaborn as sns
+
+df = pd.read_csv('SurveyLungCancer.csv')
